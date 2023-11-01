b8125d042ee66d30ef113c60ad30f99522afb5d9
Ian J. Watson <ian.james.watson@cern.ch>
2023-11-02 02:49:33 +0900

    remove vimtex stuff (use localleader)

--- lua/plugins/vimtex.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "nvim-treesitter/nvim-treesitter",
-  keys = {
-    { "<leader>t", "+vimtex" },
-    { "<leader>tt", "<cmd>VimtexTocToggle<cr>", desc = "Toc" },
-    { "<leader>ti", "<cmd>VimtexInfo<cr>", desc = "Toc" },
-    { "<leader>tb", "<cmd>VimtexCompile<cr>", desc = "Toc" },
-  },
-}
