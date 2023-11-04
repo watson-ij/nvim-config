9478cf37abaf68c4b384209306750d7ffbae28a1
Ian J. Watson <ian.james.watson@cern.ch>
2023-11-05 03:02:12 +0900

    neorg

--- lua/plugins/notes.lua
+++ lua/plugins/notes.lua
@@ -1,5 +1,29 @@
 return {
   {
+    "nvim-neorg/neorg",
+    lazy=false,
+    keys = {
+      {"<leader>n", desc="neorg"},
+      {"<leader>nn", "<cmd>Neorg<cr>"},
+    },
+    opts = {
+      load = {
+	["core.defaults"] = {},
+	["core.concealer"] = {},
+	["core.dirman"] = {
+	  config = {
+	    workspaces = {
+	      notes = "~/obsidian-vault"
+	    },
+	    default_workspace = "notes",
+	  }
+	}
+      }
+    },
+    build = {":Neorg sync-parsers"},
+    dependencies = {{"nvim-lua/plenary.nvim"}}
+  },
+  {
     "epwalsh/obsidian.nvim",
     lazy = true,
     dependencies = {
@@ -15,11 +39,18 @@ return {
       {"<leader>ot", "<cmd>ObsidianToday<CR>", desc="today's note"},
       {"<leader>oy", "<cmd>ObsidianYesterday<CR>", desc="yesterday's note"},
       {"<leader>os", "<cmd>ObsidianSearch<CR>", desc="search notes"},
+      {"<leader>ob", "<cmd>ObsidianBacklinks<CR>", desc="search notes"},
     },
     opts = {
       dir = "~/obsidian-vault",
       daily_notes = {
 	folder = "daily",
+      },
+      backlinks = {
+	-- The default height of the backlinks pane.
+	height = 10,
+	-- Whether or not to wrap lines.
+	wrap = true,
       },
     }
   }
