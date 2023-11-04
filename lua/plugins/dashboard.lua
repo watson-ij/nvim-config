5e19e1b5b454fa0c4281703b05bd63918653bb2a
Ian J. Watson <ian.james.watson@cern.ch>
2023-11-05 03:40:54 +0900

    more actions

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -9,6 +9,10 @@ return {
 	  header = {"", "", "Willkommen", "", ""},
 	  center = {
 	    {action = "Telescope find_files", desc="Find file", key="f"},
+	    {action = "Telescope oldfiles", desc="Recent files", key="r"},
+	    {action = "Telescope live_grep", desc="Grep", key="g"},
+	    {action = "Telescope find_files dir=~/.config/nvim", desc="Config", key="c"},
+	    {action = "Lazy", desc="Lazy", key="l"},
 	    {action = "qa", desc="Quit", key="q"},
 	  },
 	}
