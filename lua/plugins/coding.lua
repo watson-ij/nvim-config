d2661b095ec440cd4c31898a94640235847de499
Ian J. Watson <ian.james.watson@cern.ch>
2025-08-29 17:16:32 +0900

    surround

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,5 +1,17 @@
 return {
-  'windwp/nvim-autopairs',
-  event = "InsertEnter",
-  config = true
+  {
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    config = true
+  },
+  {
+    "kylechui/nvim-surround",
+    version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+	-- Configuration here, or leave empty to use defaults
+      })
+    end
+  }
 }
