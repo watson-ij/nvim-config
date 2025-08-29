accf5d8c996a57a5e4ba922f3786f61b208533de
Ian J. Watson <ian.james.watson@cern.ch>
2025-08-29 14:58:31 +0900

    old

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,6 +5,7 @@ return {
       {"<leader>ff", ":Telescope find_files<CR>", desc="find files"},
       {"<leader>fb", ":Telescope file_browser<CR>", desc="file browser"},
       {"<leader>fg", ":Telescope git_files<CR>", desc="git files"},
+      {"<leader>fr", ":Telescope oldfiles<CR>", desc="recent"},
       {"<leader>b", ":Telescope buffers<CR>", desc="buffers"},
       {"<leader>s", ":Telescope live_grep<CR>", desc="grep"},
     },
