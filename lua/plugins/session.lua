417a0ff14764d395fa1e1bbd719b3a8ae8360ee0
Ian J. Watson <ian.james.watson@cern.ch>
2025-08-29 15:41:17 +0900

    session

--- lua/plugins/session.lua
+++ lua/plugins/session.lua
@@ -2,11 +2,15 @@ return {
   "rmagatti/auto-session",
   lazy = false,
 
+  keys = {
+    {"<leader>p", ":AutoSession search<CR>", desc="projects"},
+  },
+
   ---enables autocomplete for opts
   ---@module "auto-session"
   ---@type AutoSession.Config
   opts = {
-    suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+    suppressed_dirs = { "~/", "~/projects", "~/Downloads", "~/sync", "~/writing", "/" },
     -- log_level = 'debug',
   },
 }
